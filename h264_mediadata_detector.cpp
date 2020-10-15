f7f43805446ab3b5fb0adfe0fc85f6eeecef9482
zjz <zjz1988314@163.com>
2020-10-16 00:53:01 +0800

    add SPS parser.

--- h264_mediadata_detector.cpp
+++ h264_mediadata_detector.cpp
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
+
 #include "bs_read.h"
 
 typedef enum {
@@ -51,6 +53,121 @@ typedef struct {
     unsigned char *buf;              //! contains the first byte followed by the EBSP
     unsigned char frame_type;        //! nalu frame type (I/P/B)
 } NALU_t;
+
+//! Boolean Type
+#ifdef FALSE
+#  define Boolean unsigned int
+#else
+typedef enum {
+  FALSE,
+  TRUE
+} Boolean;
+#endif
+
+#define MAXIMUMVALUEOFcpb_cnt   32
+typedef struct {
+  unsigned  cpb_cnt;                                            // ue(v)
+  unsigned  bit_rate_scale;                                     // u(4)
+  unsigned  cpb_size_scale;                                     // u(4)
+    unsigned  bit_rate_value [MAXIMUMVALUEOFcpb_cnt];           // ue(v)
+    unsigned  cpb_size_value[MAXIMUMVALUEOFcpb_cnt];            // ue(v)
+    unsigned  vbr_cbr_flag[MAXIMUMVALUEOFcpb_cnt];              // u(1)
+  unsigned  initial_cpb_removal_delay_length_minus1;            // u(5)
+  unsigned  cpb_removal_delay_length_minus1;                    // u(5)
+  unsigned  dpb_output_delay_length_minus1;                     // u(5)
+  unsigned  time_offset_length;                                 // u(5)
+} hrd_parameters_t;
+
+typedef struct {
+  Boolean      aspect_ratio_info_present_flag;                  // u(1)
+    unsigned  aspect_ratio_idc;                                 // u(8)
+      unsigned  sar_width;                                      // u(16)
+      unsigned  sar_height;                                     // u(16)
+  Boolean      overscan_info_present_flag;                      // u(1)
+    Boolean      overscan_appropriate_flag;                     // u(1)
+  Boolean      video_signal_type_present_flag;                  // u(1)
+    unsigned  video_format;                                     // u(3)
+    Boolean      video_full_range_flag;                         // u(1)
+    Boolean      colour_description_present_flag;               // u(1)
+      unsigned  colour_primaries;                               // u(8)
+      unsigned  transfer_characteristics;                       // u(8)
+      unsigned  matrix_coefficients;                            // u(8)
+  Boolean      chroma_location_info_present_flag;               // u(1)
+    unsigned  chroma_location_frame;                            // ue(v)
+    unsigned  chroma_location_field;                            // ue(v)
+  Boolean      timing_info_present_flag;                        // u(1)
+    unsigned  num_units_in_tick;                                // u(32)
+    unsigned  time_scale;                                       // u(32)
+    Boolean      fixed_frame_rate_flag;                         // u(1)
+  Boolean      nal_hrd_parameters_present_flag;                 // u(1)
+    hrd_parameters_t nal_hrd_parameters;                        // hrd_paramters_t
+  Boolean      vcl_hrd_parameters_present_flag;                 // u(1)
+    hrd_parameters_t vcl_hrd_parameters;                        // hrd_paramters_t
+  // if ((nal_hrd_parameters_present_flag || (vcl_hrd_parameters_present_flag))
+    Boolean      low_delay_hrd_flag;                            // u(1)
+  Boolean      bitstream_restriction_flag;                      // u(1)
+    Boolean      motion_vectors_over_pic_boundaries_flag;       // u(1)
+    unsigned  max_bytes_per_pic_denom;                          // ue(v)
+    unsigned  max_bits_per_mb_denom;                            // ue(v)
+    unsigned  log2_max_mv_length_vertical;                      // ue(v)
+    unsigned  log2_max_mv_length_horizontal;                    // ue(v)
+    unsigned  max_dec_frame_reordering;                         // ue(v)
+    unsigned  max_dec_frame_buffering;                          // ue(v)
+} vui_seq_parameters_t;
+
+#define MAXnum_ref_frames_in_pic_order_cnt_cycle  256
+typedef struct {
+  Boolean   Valid;                  // indicates the parameter set is valid
+  unsigned  profile_idc;                                        // u(8)
+  Boolean   constrained_set0_flag;                              // u(1)
+  Boolean   constrained_set1_flag;                              // u(1)
+  Boolean   constrained_set2_flag;                              // u(1)
+
+  unsigned  level_idc;                                          // u(8)
+  unsigned  seq_parameter_set_id;                               // ue(v)
+  // if( profile_idc == 100 || profile_idc == 110 ||
+  //     profile_idc == 122 || profile_idc == 144 )
+    unsigned chroma_format_idc;                                 // ue(v)
+    // if( chroma_format_idc == 3 )
+      Boolean residual_colour_transform_flag;                   // u(1)
+    unsigned bit_depth_luma_minus8;                             // ue(v)
+    unsigned bit_depth_chroma_minus8;                           // ue(v)
+    Boolean qpprime_y_zero_transform_bypass_flag;               // u(1)
+    Boolean seq_scaling_matrix_present_flag;                    // u(1)
+    // if( seq_scaling_matrix_present_flag )
+    //   for( i=0; i<8; i++)
+      Boolean seq_scaling_list_present_flag[8];                 // u(1)
+
+  unsigned  log2_max_frame_num_minus4;                          // ue(v)
+  unsigned  pic_order_cnt_type;
+  // if( pic_order_cnt_type == 0 )
+    unsigned log2_max_pic_order_cnt_lsb_minus4;                 // ue(v)
+  // else if( pic_order_cnt_type == 1 )
+    Boolean delta_pic_order_always_zero_flag;                   // u(1)
+    int     offset_for_non_ref_pic;                             // se(v)
+    int     offset_for_top_to_bottom_field;                     // se(v)
+    unsigned  num_ref_frames_in_pic_order_cnt_cycle;            // ue(v)
+    // for( i=0; i<num_ref_frames_in_pic_order_cnt_cycle; i++ )
+      int   offset_for_ref_frame[MAXnum_ref_frames_in_pic_order_cnt_cycle];   // se(v)
+  unsigned  num_ref_frames;                                     // ue(v)
+  Boolean   gaps_in_frame_num_value_allowed_flag;               // u(1)
+  unsigned  pic_width_in_mbs_minus1;                            // ue(v)
+  unsigned  pic_height_in_map_units_minus1;                     // ue(v)
+  Boolean   frame_mbs_only_flag;                                // u(1)
+  // if( !frame_mbs_only_flag )
+    Boolean   mb_adaptive_frame_field_flag;                     // u(1)
+  Boolean   direct_8x8_inference_flag;                          // u(1)
+  Boolean   frame_cropping_flag;                                // u(1)
+  // if( frame_cropping_flag )
+    unsigned  frame_cropping_rect_left_offset;                  // ue(v)
+    unsigned  frame_cropping_rect_right_offset;                 // ue(v)
+    unsigned  frame_cropping_rect_top_offset;                   // ue(v)
+    unsigned  frame_cropping_rect_bottom_offset;                // ue(v)
+  Boolean   vui_parameters_present_flag;                        // u(1)
+  // if( vui_parameters_present_flag )
+    vui_seq_parameters_t vui_seq_parameters;                    //
+} seq_parameter_set_rbsp_t;
+
 
 FILE *h264bitstream = NULL;                // the bit stream file
 
@@ -187,6 +304,128 @@ static int GetFrameType(NALU_t * nal)
     return 0;
 }
 
+static int ParseAndDumpSPSInfo(NALU_t * nal)
+{
+    bs_t s;
+    seq_parameter_set_rbsp_t sps;
+    FILE *myout=stdout;
+
+    bs_init(&s, nal->buf+1, nal->len - 1);
+
+    printf("---------------------SPS info--------------------------\n");
+    sps.profile_idc = bs_read(&s, 8);
+    fprintf(myout, "profile_idc:                            %d\n", sps.profile_idc);
+
+    sps.constrained_set0_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "constrained_set0_flag:                  %d\n", sps.constrained_set0_flag);
+    sps.constrained_set1_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "constrained_set1_flag:                  %d\n", sps.constrained_set1_flag);
+    sps.constrained_set2_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "constrained_set2_flag:                  %d\n", sps.constrained_set2_flag);
+    int reserved_zero = bs_read(&s, 5);
+    fprintf(myout, "reserved_zero:                          %d\n", reserved_zero);
+    assert(reserved_zero == 0);
+
+    sps.level_idc = bs_read(&s, 8);
+    fprintf(myout, "level_idc:                              %d\n", sps.level_idc);
+    sps.seq_parameter_set_id = bs_read_ue(&s);
+    fprintf(myout, "seq_parameter_set_id:                   %d\n", sps.seq_parameter_set_id);
+
+    if (sps.profile_idc==100 || sps.profile_idc==110 || sps.profile_idc==122 || sps.profile_idc==144) {
+        sps.chroma_format_idc = bs_read_ue(&s);
+        fprintf(myout, "  chroma_format_idc:                          %d\n", sps.chroma_format_idc);
+        if (sps.chroma_format_idc == 3) {
+            sps.residual_colour_transform_flag = (Boolean)bs_read1(&s);
+            fprintf(myout, "    residual_colour_transform_flag:                   %d\n", sps.residual_colour_transform_flag);
+        }
+        sps.bit_depth_luma_minus8 = bs_read_ue(&s);
+        fprintf(myout, "  bit_depth_luma_minus8:                      %d\n", sps.bit_depth_luma_minus8);
+        sps.bit_depth_chroma_minus8 = bs_read_ue(&s);
+        fprintf(myout, "  bit_depth_chroma_minus8:                    %d\n", sps.bit_depth_chroma_minus8);
+        sps.qpprime_y_zero_transform_bypass_flag = (Boolean)bs_read1(&s);
+        fprintf(myout, "  qpprime_y_zero_transform_bypass_flag:       %d\n", sps.qpprime_y_zero_transform_bypass_flag);
+        sps.seq_scaling_matrix_present_flag = (Boolean)bs_read1(&s);
+        fprintf(myout, "  seq_scaling_matrix_present_flag:            %d\n", sps.seq_scaling_matrix_present_flag);
+        if (sps.seq_scaling_matrix_present_flag) {
+            for (int i=0; i<8; i++) {
+                sps.seq_scaling_list_present_flag[i] = (Boolean)bs_read1(&s);
+                fprintf(myout, "    seq_scaling_list_present_flag:                %d\n", sps.seq_scaling_list_present_flag[i]);
+            }
+        }
+    }
+
+    sps.log2_max_frame_num_minus4 = bs_read_ue(&s);
+    fprintf(myout, "log2_max_frame_num_minus4:              %d\n", sps.log2_max_frame_num_minus4);
+
+    sps.pic_order_cnt_type = bs_read_ue(&s);
+    fprintf(myout, "pic_order_cnt_type:                     %d\n", sps.pic_order_cnt_type);
+    if (sps.pic_order_cnt_type == 0) {
+        sps.log2_max_pic_order_cnt_lsb_minus4 = bs_read_ue(&s);
+        fprintf(myout, "  log2_max_pic_order_cnt_lsb_minus4:          %d\n", sps.log2_max_pic_order_cnt_lsb_minus4);
+    } else if (sps.pic_order_cnt_type == 1) {
+        sps.delta_pic_order_always_zero_flag = (Boolean)bs_read1(&s);
+        //sps.offset_for_non_ref_pic = se_v(&s);
+        //sps.offset_for_top_to_bottom_field = se_v(&s);
+        sps.num_ref_frames_in_pic_order_cnt_cycle = bs_read_ue(&s);
+        for (int i=0; i<sps.num_ref_frames_in_pic_order_cnt_cycle; i++)
+            ;
+            //sps.offset_for_ref_frame[i] = se_v(&s);
+    }
+
+    sps.num_ref_frames = bs_read_ue(&s);
+    fprintf(myout, "num_ref_frames:                         %d\n", sps.num_ref_frames);
+    sps.gaps_in_frame_num_value_allowed_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "gaps_in_frame_num_value_allowed_flag:   %d\n", sps.gaps_in_frame_num_value_allowed_flag);
+
+    sps.pic_width_in_mbs_minus1 = bs_read_ue(&s);
+    fprintf(myout, "pic_width_in_mbs_minus1:                %d\n", sps.pic_width_in_mbs_minus1);
+    sps.pic_height_in_map_units_minus1 = bs_read_ue(&s);
+    fprintf(myout, "pic_height_in_map_units_minus1:         %d\n", sps.pic_height_in_map_units_minus1);
+
+    sps.frame_mbs_only_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "frame_mbs_only_flag:                    %d\n", sps.frame_mbs_only_flag);
+    if (!sps.frame_mbs_only_flag) {
+        sps.mb_adaptive_frame_field_flag = (Boolean)bs_read1(&s);
+        fprintf(myout, "  mb_adaptive_frame_field_flag:               %d\n", sps.mb_adaptive_frame_field_flag);
+    }
+    sps.direct_8x8_inference_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "direct_8x8_inference_flag:              %d\n", sps.direct_8x8_inference_flag);
+
+    sps.frame_cropping_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "frame_cropping_flag:                    %d\n", sps.frame_cropping_flag);
+    if (sps.frame_cropping_flag) {
+        sps.frame_cropping_rect_left_offset = bs_read_ue(&s);
+        fprintf(myout, "  frame_cropping_rect_left_offset:            %d\n", sps.frame_cropping_rect_left_offset);
+        sps.frame_cropping_rect_right_offset = bs_read_ue(&s);
+        fprintf(myout, "  frame_cropping_rect_right_offset:           %d\n", sps.frame_cropping_rect_right_offset);
+        sps.frame_cropping_rect_top_offset = bs_read_ue(&s);
+        fprintf(myout, "  frame_cropping_rect_top_offset:             %d\n", sps.frame_cropping_rect_top_offset);
+        sps.frame_cropping_rect_bottom_offset = bs_read_ue(&s);
+        fprintf(myout, "  frame_cropping_rect_bottom_offset:          %d\n", sps.frame_cropping_rect_bottom_offset);
+    }
+
+    // vui
+    sps.vui_parameters_present_flag = (Boolean)bs_read1(&s);
+    fprintf(myout, "vui_parameters_present_flag:            %d\n", sps.vui_parameters_present_flag);
+    if (sps.vui_parameters_present_flag) {
+        printf ("VUI sequence parameters present but not supported, ignored, proceeding to next NALU\n");
+    }
+    sps.Valid = TRUE;
+    printf("-------------------------------------------------------\n");
+
+    return 0;
+}
+
+static int ParseAndDumpPPSInfo(NALU_t * nal)
+{
+    return 0;
+}
+
+static int ParseAndDumpSEIInfo(NALU_t * nal)
+{
+    return 0;
+}
+
 /**
  * Analysis H.264 Bitstream
  * @param url    Location of input H.264 bitstream file.
@@ -267,6 +506,19 @@ int simplest_h264_parser(char *url){
 
         data_offset = data_offset + data_lenth;
         nal_num++;
+
+        // parse sps & pps & sei
+        switch(nalu->nal_unit_type) {
+            case NALU_TYPE_SPS:
+                ParseAndDumpSPSInfo(nalu);
+                break;
+            case NALU_TYPE_PPS:
+                ParseAndDumpPPSInfo(nalu);
+                break;
+            case NALU_TYPE_SEI:
+                ParseAndDumpSEIInfo(nalu);
+                break;
+        }
     }
 
     if (nalu) {
