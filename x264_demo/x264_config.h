dd1ff6fa6094ac433c7a85b322789804144e023e
zjz <zjz1988314@163.com>
2020-12-25 00:16:01 +0800

    x264_demo: base on x264 project.

---
+++ x264_demo/x264_config.h
@@ -0,0 +1,8 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT 0
+#define X264_REV 3027
+#define X264_REV_DIFF 0
+#define X264_VERSION " r3027 4121277"
+#define X264_POINTVER "0.161.3027 4121277"
